8d12d80a3cdff8af361d424e7ded81b66aaf899b
hosenA51 <hosenahmed2552@gmail.com>
2025-01-05 18:23:38 +0600

    change website title

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -25,7 +25,7 @@ const Contact = () => {
                 Contact Me
             </h2>
             <div className='md:flex gap-4 items-center justify-center'>
-                <div className='card bg-gray-600 w-full max-w-sm shrink-0 shadow-2xl ml-5'>
+                <div className='card bg-gray-600 w-full max-w-sm shrink-0 shadow-2xl ml-5 md:ml-0'>
                     <form className='card-body' ref={form} onSubmit={sendEmail}>
                         <label className="label-text text-white">Name</label>
                         <input className='input input-bordered text-white bg-gray-700' type="text" name="user_name" />
@@ -38,7 +38,7 @@ const Contact = () => {
                 </div>
                 <div className='hidden md:flex w-1 mx-8 h-80 border-2 text-white mb-2'>
                 </div>
-                <div className='space-y-5 flex flex-col justify-center mt-6 ml-5'>
+                <div className='space-y-5 flex flex-col justify-center mt-6 ml-5 md:ml-0'>
                     <h1 className='text-4xl text-[#FF6B00] font-bold'>Hosen Ahmed</h1>
                     <p className='text-lg font-semibold text-white'>Address: Moulvibazar, Sylhet, Bangladesh</p>
                     <p className='text-lg font-semibold text-white'>Emall: hosenahmed2552@gmail.com</p>
